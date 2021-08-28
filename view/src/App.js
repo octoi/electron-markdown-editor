67d43ecfd8688a5c94e308b4e85d03c520446546
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-28 20:41:07 +0530

    basic editor

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MarkdownToJsx from 'markdown-to-jsx';
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('');
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className='app'>
+      <textarea
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+        placeholder='Enter in markdown'
+      />
+      <MarkdownToJsx>{markdown}</MarkdownToJsx>
     </div>
   )
 }
